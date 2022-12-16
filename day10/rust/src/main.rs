e01ab3f9cc2ad11d25fc8c419bea27e87d98f80b
Cody Arnold <carnold@bluebeam.com>
2022-12-16 17:19:53 -0600

    (wip) Iterate through all, working on returning single integer

--- day10/rust/src/main.rs
+++ day10/rust/src/main.rs
@@ -13,10 +13,9 @@ impl FromStr for Command {
     fn from_str(s: &str) -> Result<Self, Self::Err> {
         let mut parts = s.split_whitespace();
         let cmd = parts.next().unwrap();
-        let arg = parts.next().unwrap();
         match cmd {
             "noop" => Ok(Command::Noop),
-            "addx" => Ok(Command::Addx(arg.parse().unwrap())),
+            "addx" => Ok(Command::Addx(parts.next().unwrap().parse().unwrap())),
             _ => Err(format!("Unknown command: {}", cmd))
         }
     }
@@ -26,9 +25,41 @@ fn main() {
     // get string input from file
     let input = include_str!("./input1.txt");
 
+    let mut cycle = 1;
+    let mut signal_strength = 0;
+    let mut x_value = 0;
+
     let output = input
         .lines()
-        .map(|line| line.parse::<Command>().unwrap());
+        .map(|line| line.parse::<Command>().unwrap())
+        .collect::<Vec<Command>>()
+        .iter()
+        .reduce(|command, signal_strength| {
+            match command {
+                Command::Noop => {
+                    if at_cycle_checkpoint(&cycle) {
+                        signal_strength += signal_strength + (x_value * cycle);
+                    }
+                },
+                Command::Addx(x) => {
+                    if at_cycle_checkpoint(&cycle) {
+                        signal_strength += signal_strength + (x_value * cycle);
+                    }
+                    cycle += 1;
+                    if at_cycle_checkpoint(&cycle) {
+                        signal_strength += signal_strength + (x_value * cycle);
+                    }
+                    x_value += x;
+                }
+            }
+
+            cycle += 1;
+            return signal_strength;
+        });
 
     println!("{:?}", output);
+}
+
+fn at_cycle_checkpoint(cycle: &i32) -> bool {
+    *cycle == 20 || (*cycle - 20) % 40 == 0
 }
